f0e3d702a35525a539cbd09c8fa1d4e4b8ff8146
Farza <contactclothinghousenow@gmail.com>
2016-02-06 06:16:12 -0500

    Added Menu State

--- core/src/com/mygdx/game/States/MenuState.java
+++ core/src/com/mygdx/game/States/MenuState.java
@@ -1,7 +1,46 @@
 package com.mygdx.game.States;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 /**
  * Created by m on 2/6/16.
  */
-public class MenuState {
+public class MenuState extends State {
+
+    private Texture background;
+
+    public MenuState(GSM gsm) {
+        super(gsm);
+        background = new Texture("purple.jpg");
+    }
+
+    public void handleInput() {
+        //Should make this able to communicate with LeapMotion.
+        if (Gdx.input.justTouched()) {
+            //DO THIS--> gsm.set(new PlayState(gsm));
+            //Should add this later if we have images -->
+            //dispose();
+        }
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background, 0, 0, 500, 500);
+        sb.end();
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
 }
--- core/src/com/mygdx/game/States/State.java
+++ core/src/com/mygdx/game/States/State.java
@@ -16,7 +16,7 @@ public abstract class State {
         this.gsm = gsm;
     }
 
-    public abstract void handleInput();
+    protected abstract void handleInput();
     public abstract void update(float dt);
     public abstract void render(SpriteBatch sb);
     public abstract void dispose();
